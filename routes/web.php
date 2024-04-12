980f0e955a4974861230b2a2e60fd85028966da7
Brian Chebon <brianlchebon@gmail.com>
2024-04-12 10:39:53 +0300

    added logout controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,4 +10,5 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
 });
