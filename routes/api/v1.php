7fcb6a049a064c3fa2f760aaa11a5cbcb12d0d57
Brian Chebon <brianlchebon@gmail.com>
2024-04-15 09:19:40 +0300

    create V2 links and checked on postman

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 
-Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+Route::prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
